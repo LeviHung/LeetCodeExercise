4a065ff0cb2eb128c509555db3d33e294c53d5eb
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-07-09 18:34:32 +0000

    Add a CountCompleteTreeNodes class to count the number of the nodes in the tree.

--- src/LeetCodeExercise.java
+++ src/LeetCodeExercise.java
@@ -61,6 +61,7 @@ import easy.IsomorphicStrings;
 import easy.ReverseLinkedList;
 import easy.ContainsDuplicate;
 import easy.ContainsDuplicateII;
+import easy.CountCompleteTreeNodes;
 
 class LeetCodeExercise 
 {
@@ -247,6 +248,9 @@ class LeetCodeExercise
               case 219:
                 ContainsDuplicateII.main();
                 break;
+              case 222:
+                CountCompleteTreeNodes.main();
+                break;
               default:
                 if (subKey != 0) {
                   System.out.println("Invalid Input!");
@@ -393,6 +397,7 @@ class LeetCodeExercise
     System.out.println("206. Reverse Linked List");
     System.out.println("217. Contains Duplicate");
     System.out.println("219. Contains Duplicate II");
+    System.out.println("222. Count Complete Tree Nodes");
     System.out.println("0. Go Back");
     System.out.println("----------------------------------------");
     System.out.print("Enter a choice: ");
