631f284f803cc36df3c233eedb2f9167c2e5d3b8
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-07-17 05:28:41 +0000

    Add an item of the premium menu for Valid Word Square.

--- src/LeetCodeExercise.java
+++ src/LeetCodeExercise.java
@@ -566,6 +566,9 @@ class LeetCodeExercise
               case 408:
                 System.out.println("Subscribe to unlock.");
                 break;
+              case 422:
+                System.out.println("Subscribe to unlock.");
+                break;
               default:
                 if (subKey != 0) {
                   System.out.println("Invalid Input!");
@@ -780,6 +783,7 @@ class LeetCodeExercise
     System.out.println("346. Moving Average From Data Stream");
     System.out.println("359. Logger Rate Limiter");
     System.out.println("408. Valid Word Abbreviation");
+    System.out.println("422. Valid Word Square");
     System.out.println("0. Go Back");
     System.out.println("--------------------------------------");
     System.out.print("Enter a choice: ");
