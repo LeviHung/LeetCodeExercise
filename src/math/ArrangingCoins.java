f0dd1ab251aab7ed0b464817502a188aa3b0cfc8
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-07-17 17:58:08 +0000

    Add an extra method of ArrangingCoins class.

--- src/math/ArrangingCoins.java
+++ src/math/ArrangingCoins.java
@@ -39,6 +39,24 @@ public class ArrangingCoins
     return (int) ((double) (1 + n) * (double) n / (double) 2);
   }
 
+  public int arrangeCoinsEx(int n) 
+  {
+      long left = 0, right = n;
+      long k, curr;
+      while (left <= right) {
+          k = left + (right - left) / 2;
+          curr = k * (k + 1) / 2;
+
+          if (curr == n) return (int)k;
+
+          if (n < curr) {
+              right = k - 1;
+          } else {
+              left = k + 1;
+          }
+      }
+      return (int)right;
+  }
   public int arrangeCoinsOpt(int n) 
   {
     double kt = 2.0 * n + 0.25;
