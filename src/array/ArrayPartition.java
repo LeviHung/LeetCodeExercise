3f82aa7f2fc2c25b494c4f1fc465844b190a2c65
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-07-31 05:37:37 +0000

    Fix the tab problem in the ArrayPartition class.

--- src/array/ArrayPartition.java
+++ src/array/ArrayPartition.java
@@ -1,7 +1,7 @@
 package array;
 import java.util.Arrays;
 
-public class ArrayPartition
+public class AssignCookies
 {
   public static int arrayPairSum(int[] nums)
   {
@@ -19,25 +19,25 @@ public class ArrayPartition
     int max = Integer.MIN_VALUE;
     int min = Integer.MAX_VALUE;
     for (int num : nums) {
-        max = Math.max(num, max);
-        min = Math.min(num, min);
+      max = Math.max(num, max);
+      min = Math.min(num, min);
     }
     int[] counts = new int[max - min + 1];
     for (int num : nums) {
-        counts[num - min]++;
+      counts[num - min]++;
     }
     int res = 0;
     int idx = 0;
     for (int i = 0; i < nums.length / 2; i++) {
-        while (counts[idx] == 0) {
-            idx++;
-        }
-        res += idx;
-        counts[idx]--;
-        while (counts[idx] == 0) {
-            idx++;
-        }
-        counts[idx]--;
+      while (counts[idx] == 0) {
+        idx++;
+      }
+      res += idx;
+      counts[idx]--;
+      while (counts[idx] == 0) {
+        idx++;
+      }
+      counts[idx]--;
     }
     return res + nums.length / 2 * min;
   }
