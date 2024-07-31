437db00a887102d6ecdee18d45a34c0aed257ae4
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-07-31 05:33:50 +0000

    Fix the tab problem in the ArrayPartition class.

--- src/array/ArrayPartition.java
+++ src/array/ArrayPartition.java
@@ -16,32 +16,32 @@ public class ArrayPartition
 
   public static int arrayPairSumOpt(int[] nums) 
   {
-      int max = Integer.MIN_VALUE;
-      int min = Integer.MAX_VALUE;
-      for (int num : nums) {
-          max = Math.max(num, max);
-          min = Math.min(num, min);
-      }
-      int[] counts = new int[max - min + 1];
-      for (int num : nums) {
-          counts[num - min]++;
-      }
-      int res = 0;
-      int idx = 0;
-      for (int i = 0; i < nums.length / 2; i++) {
-          while (counts[idx] == 0) {
-              idx++;
-          }
-          res += idx;
-          counts[idx]--;
-          while (counts[idx] == 0) {
-              idx++;
-          }
-          counts[idx]--;
-      }
-      return res + nums.length / 2 * min;
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+    for (int num : nums) {
+        max = Math.max(num, max);
+        min = Math.min(num, min);
+    }
+    int[] counts = new int[max - min + 1];
+    for (int num : nums) {
+        counts[num - min]++;
+    }
+    int res = 0;
+    int idx = 0;
+    for (int i = 0; i < nums.length / 2; i++) {
+        while (counts[idx] == 0) {
+            idx++;
+        }
+        res += idx;
+        counts[idx]--;
+        while (counts[idx] == 0) {
+            idx++;
+        }
+        counts[idx]--;
+    }
+    return res + nums.length / 2 * min;
   }
-  
+
   public static void readme()
   {
     String str = "Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.\n";
