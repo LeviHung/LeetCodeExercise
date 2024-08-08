a0da46d9b8b94c9c80697265f99b3f411ac3eb88
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-08-08 19:02:13 +0000

    Fix the tab problem in the FlippingImage class.

--- src/matrix/FlippingImage.java
+++ src/matrix/FlippingImage.java
@@ -8,17 +8,17 @@ public class FlippingImage
     int [][] res = new int[image.length][image[0].length];
     int cIndex;
     for (int r = 0; r < image.length; r++) {
-        cIndex = image[0].length - 1;
-        for (int c = 0; c < image[0].length; c++) {
-            res[r][c] = image[r][cIndex--] ^ 1; 
-            /*
-            if (image[r][cIndex--] == 0) {
-                res[r][c] = 1;
-            } else {
-                res[r][c] = 0;
-            }
-            */
+      cIndex = image[0].length - 1;
+      for (int c = 0; c < image[0].length; c++) {
+        res[r][c] = image[r][cIndex--] ^ 1; 
+        /*
+        if (image[r][cIndex--] == 0) {
+            res[r][c] = 1;
+        } else {
+            res[r][c] = 0;
         }
+        */
+      }
     }
     return res;
   }
@@ -28,7 +28,7 @@ public class FlippingImage
     String str = "Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.\n\n";
 
     str += "To flip an image horizontally means that each row of the image is reversed.\n\n";
-    
+
     str += "For example, flipping [1,1,0] horizontally results in [0,1,1].\n\n";
 
     str += "To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.\n\n";
