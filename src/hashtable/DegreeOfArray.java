93e57439825a806aa677fac02e44235fe9599e55
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-08-13 02:44:23 +0000

    Fix the tab problem in the DegreeOfArray class.

--- src/hashtable/DegreeOfArray.java
+++ src/hashtable/DegreeOfArray.java
@@ -12,10 +12,10 @@ public class DegreeOfArray
     for (int i = 0; i < nums.length; i++) {
       array[0][nums[i]]++;
       if (array[0][nums[i]] == 1) {
-          array[1][nums[i]] = i;
-          array[2][nums[i]] = i;
+        array[1][nums[i]] = i;
+        array[2][nums[i]] = i;
       } else {
-          array[2][nums[i]] = i;
+        array[2][nums[i]] = i;
       }
     }
     int max = 0;
@@ -24,16 +24,17 @@ public class DegreeOfArray
     for (int i = 0; i < array[0].length && total < nums.length; i++) {
       total += array[0][i];
       if (array[0][i] > max) {
-          max = array[0][i];
-          min = 50000;
-          if (array[2][i] - array[1][i] < min)
-              min = array[2][i] - array[1][i];
+        max = array[0][i];
+        min = 50000;
+        if (array[2][i] - array[1][i] < min)
+          min = array[2][i] - array[1][i];
+        
       } else if (array[0][i] == max && array[0][i] != 0) {
-          if (array[2][i] - array[1][i] < min)
-              min = array[2][i] - array[1][i];
+        if (array[2][i] - array[1][i] < min)
+          min = array[2][i] - array[1][i];
       }
     }
-    
+
     return min + 1;
   }
 
@@ -46,18 +47,18 @@ public class DegreeOfArray
     MinMax [] minmax = new MinMax[max + 1];
     int maxfreq = 0;
     for (int i=0; i<nums.length;i++) {
-        if (minmax[nums[i]] == null)
-            minmax[nums[i]] = new MinMax(i);
-        minmax[nums[i]].max = i;
-        minmax[nums[i]].count++;
+      if (minmax[nums[i]] == null)
+        minmax[nums[i]] = new MinMax(i);
+      minmax[nums[i]].max = i;
+      minmax[nums[i]].count++;
 
-        if (minmax[nums[i]].count > maxfreq)
-            maxfreq = minmax[nums[i]].count;
+      if (minmax[nums[i]].count > maxfreq)
+        maxfreq = minmax[nums[i]].count;
     }
     int minarray = Integer.MAX_VALUE;
     for (int i=0; i<minmax.length;i++) {
-        if (minmax[i] != null && minmax[i].count == maxfreq)
-            minarray = Math.min(minarray, minmax[i].max - minmax[i].min + 1);
+      if (minmax[i] != null && minmax[i].count == maxfreq)
+        minarray = Math.min(minarray, minmax[i].max - minmax[i].min + 1);
     }
     return minarray;
   }
@@ -86,18 +87,18 @@ public class DegreeOfArray
   {
     DegreeOfArray.readme();
 
-    // nums = [1,2,2,3,1] => 2
-    int [] nums1 = {1,2,2,3,1};
-    System.out.println("findShortestSubArray(" + Arrays.toString(nums1) + ") = " +
-                       findShortestSubArray(nums1));
+      // nums = [1,2,2,3,1] => 2
+      int [] nums1 = {1,2,2,3,1};
+      System.out.println("findShortestSubArray(" + Arrays.toString(nums1) + ") = " +
+                         findShortestSubArray(nums1));
 
-    // nums = [1,2,2,3,1,4,2] => 6
-   int [] nums2 = {1,2,2,3,1,4,2};
-    System.out.println("findShortestSubArray(" + Arrays.toString(nums2) + ") = " +
-                       findShortestSubArray(nums2));
+      // nums = [1,2,2,3,1,4,2] => 6
+     int [] nums2 = {1,2,2,3,1,4,2};
+      System.out.println("findShortestSubArray(" + Arrays.toString(nums2) + ") = " +
+                         findShortestSubArray(nums2));
 
-    System.out.println("findShortestSubArrayOpt(" + Arrays.toString(nums2) + ") = " +
-     findShortestSubArrayOpt(nums2));
+      System.out.println("findShortestSubArrayOpt(" + Arrays.toString(nums2) + ") = " +
+       findShortestSubArrayOpt(nums2));
 
   }
 }
